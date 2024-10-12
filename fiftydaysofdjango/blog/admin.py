0bf11e12d645e22cffd3238d437b07d4c323c805
Andres <andreservio@gmail.com>
2024-10-12 11:42:54 -0300

    Añadí en la pantalla de administrador la posibilidad de ver y filtrar el campo sponsor.

--- fiftydaysofdjango/blog/admin.py
+++ fiftydaysofdjango/blog/admin.py
@@ -4,8 +4,8 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ["title", "slug", "author", "publish", "status"]
-    list_filter = ['status', 'created', 'publish', 'author']
+    list_display = ["title", "slug", "author", "publish", "status", "sponsor"]
+    list_filter = ['status', 'created', 'publish', 'author', "sponsor"]
     search_fields = ['title', 'body']
     date_hierarchy = 'publish'
     prepopulated_fields = {'slug' : ('title', )}
