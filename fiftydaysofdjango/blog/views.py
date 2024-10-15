723ffc7c5c83fc222aed6106852c9462a116a845
Andres <andreservio@gmail.com>
2024-10-15 17:09:27 -0300

    añadida la paginacion, resta corregir el funcionamiento de la vista de la lista

--- fiftydaysofdjango/blog/views.py
+++ fiftydaysofdjango/blog/views.py
@@ -1,18 +1,29 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 
 
+
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 5)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+
     return render(
-        request, "blog/post/list.html", {'posts' : posts}
+        request,
+        "blog/post/list.html",
+        {'posts' : posts}
     )
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
-        id =id,
-        status= Post.Status.PUBLISHED)
+        status= Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day)
     return render(
         request, 
         'blog/post/detail.html', 
