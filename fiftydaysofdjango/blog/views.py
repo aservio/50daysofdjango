d8fb60f5cf29ab059cf449403750fd1429acdfb7
Andres <andreservio@gmail.com>
2024-11-19 17:05:23 -0300

    Añadido sistema de comentarios

--- fiftydaysofdjango/blog/views.py
+++ fiftydaysofdjango/blog/views.py
@@ -31,10 +31,17 @@ def post_detail(request, year, month, day, post):
         publish__year=year,
         publish__month=month,
         publish__day=day)
+    
+    comments = post.comments.filter(active = True)
+    form = CommentForm()
     return render(
         request, 
         'blog/post/detail.html', 
-        {'post': post}
+        {
+            'post': post,
+            'comments' : comments,
+            'form' : form
+         }
     )
 
 class PostListView(ListView):
